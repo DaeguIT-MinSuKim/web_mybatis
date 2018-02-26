8ac4fd8928d5de4356f174fa9efab9e9009a1611
MinSu-Kim <net94.teacher73@gmail.com>
2018-02-26 21:16:33 +0900

    web project initial

---
+++ src/test/kr/or/dgit/web_mybatis/MyBatisSqlSessionFactoryTest.java
@@ -0,0 +1,40 @@
+package kr.or.dgit.web_mybatis;
+
+import java.sql.Connection;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import kr.or.dgit.web_mybatis.util.MyBatisSqlSessionFactory;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class MyBatisSqlSessionFactoryTest {
+	private static SqlSessionFactory factory;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		factory = MyBatisSqlSessionFactory.getSqlSessionFactory();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		factory = null;
+	}
+
+	@Test
+	public void testFactory() {
+		Assert.assertNotNull(factory);
+	}
+	
+	@Test
+	public void testSession() {
+		Connection connection = factory.openSession().getConnection();
+		Assert.assertNotNull(connection);
+	}
+
+}
